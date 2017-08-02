ba15468ad699252569d3b20508f31f67df097b5d
Marty Corbett <corbe054@umn.edu>
2017-08-02 21:22:40 +0000

    The reactJS tutorial is working up until the last part

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,14 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var RoomsList = React.createClass({
-    loadRoomsFromServer: function(){
-        $.ajax({
-            url: this.props.url,
-            datatype: 'json',
-            cache: false,
-            success: function(data) {
-                this.setState({data: data});
-            }.bind(this)
-        })
-    },
-
-    getInitialState: function() {
-        return {data: []};
-    },
-
-    componentDidMount: function() {
-        this.loadRoomsFromServer();
-        setInterval(this.loadRoomsFromServer, 
-                    this.props.pollInterval)
-    }, 
+var Hello = React.createClass ({
     render: function() {
-        if (this.state.data) {
-            console.log('DATA!')
-            var roomNodes = this.state.data.map(function(room){
-                return <li> {room.code} </li>
-            })
-        }
         return (
-            <div>
-                <h1>Hello React!</h1>
-                <ul>
-                    {roomNodes}
-                </ul>
-            </div>
+            <h1>
+            Hello, React!
+            </h1>
         )
     }
 })
 
-ReactDOM.render(<RoomsList url='/api/v1/room' pollInterval={1000} />, 
-    document.getElementById('container'))
+ReactDOM.render(<Hello />, document.getElementById('container'))
