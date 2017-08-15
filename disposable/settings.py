794a6b70ad3ad0c77b45e0a72b015e4047eb1461
Marty Corbett <corbe054@umn.edu>
2017-08-15 15:43:23 +0000

    Add local jQuery package

--- disposable/settings.py
+++ disposable/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = (
     # This lets Django's collectstatic store webpack bundles
