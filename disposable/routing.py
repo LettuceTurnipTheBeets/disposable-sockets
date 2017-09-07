49d89c0cb6a02896be14fd49a1700c7604ad5658
Marty Corbett <corbe054@umn.edu>
2017-09-07 18:36:05 +0000

    Cleanup comments

--- disposable/routing.py
+++ disposable/routing.py
@@ -1,23 +1,13 @@
 from channels import route
 from sockets.consumers import save_post, connect_chat, disconnect_chat
 
-
-# The channel routing defines what channels get handled by what consumers,
-# including optional matching on message attributes. In this example, we route
-# all WebSocket connections to the class-based BindingConsumer (the consumer
-# class itself specifies what channels it wants to consume)
 channel_routing = [
-    # Called when incoming WebSockets connect - liveblog
+    # Called when incoming WebSockets connect
     route("websocket.connect", connect_chat, path=r'^/(?P<code>[a-zA-Z]{4})/stream/$'),
 
-    # Called when the client closes the socket - liveblog
+    # Called when the client closes the socket
     route("websocket.disconnect", disconnect_chat, path=r'^(?P<code>[a-zA-Z]{4})/stream/$'),
 
-    # Called when the client sends message on the WebSocket - liveblog
+    # Called when the client sends message on the WebSocket
     route("websocket.receive", save_post, path=r'^/(?P<code>[a-zA-Z]{4})/stream/$'),
-
-    # A default "http.request" route is always inserted by Django at the end of the routing list
-    # that routes all unmatched HTTP requests to the Django view system. If you want lower-level
-    # HTTP handling - e.g. long-polling - you can do it here and route by path, and let the rest
-    # fall through to normal views.
 ]
