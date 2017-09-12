eaf213a12c243883dcf8e8797a66debd576513ac
Marty Corbett <corbe054@umn.edu>
2017-09-12 21:04:50 +0000

    Switched serving static files from /assets/ to /static/

--- disposable/settings.py
+++ disposable/settings.py
@@ -135,9 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- sockets/models/chat.py
+++ sockets/models/chat.py
@@ -13,7 +13,7 @@ class Chat(models.Model):
     room = models.ForeignKey('sockets.Room', db_column='room_id', related_name='chat')
     name = models.CharField(max_length=40)
     message = models.TextField(max_length=140)
-    drawing = models.ImageField(upload_to='assets/images/', default='assets/images/no-img.jpg')
+    drawing = models.ImageField(upload_to='static/images/', default='static/images/no-img.jpg')
     time = models.DateTimeField(blank=False)
 
     def __str__(self):
