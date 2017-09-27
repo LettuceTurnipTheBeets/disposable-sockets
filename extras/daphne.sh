364e6253de526d8fbb2473b356e3ebd4feb85f9e
Marty Corbett <corbe054@umn.edu>
2017-09-27 15:34:36 +0000

    Update daphne and worker scripts

--- extras/daphne.sh
+++ extras/daphne.sh
@@ -1,10 +1,5 @@
-start on runlevel [2345]
-stop on runlevel [016]
+#!/bin/bash
+cd /home/corbe054/disposable-sockets/
+source /var/env/disposable/bin/activate
+daphne -b 0.0.0.0 -p 8000 disposable.asgi:channel_layer
 
-respawn
-
-script
-    cd /home/corbe054/disposable-sockets/
-    export DJANGO_SETTINGS_MODULE="disposable.settings"
-    exec daphne -b 0.0.0.0 -p 8000 disposable.asgi:channel_layer
-end script
--- extras/worker.sh
+++ extras/worker.sh
@@ -1,10 +1,4 @@
-start on runlevel [2345]
-stop on runlevel [016]
-
-respawn
-
-script
-    cd /home/corbe054/disposable-sockets/
-    export DJANGO_SETTINGS_MODULE="disposable.settings"
-    exec python manage.py runworker
-end script
+#!/bin/bash
+cd /home/corbe054/disposable-sockets/
+source /var/env/disposable/bin/activate
+python manage.py runworker
