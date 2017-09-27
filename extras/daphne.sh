273a4d2e186a63658dbb05fbbebe98d071793dbc
Marty Corbett <corbe054@umn.edu>
2017-09-27 16:51:18 +0000

    Update scripts

--- extras/daphne.sh
+++ extras/daphne.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Place in /etc/rc.local
 cd /home/corbe054/disposable-sockets/
 source /var/env/disposable/bin/activate
 daphne -b 0.0.0.0 -p 8000 disposable.asgi:channel_layer
---
+++ extras/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Place in /etc/rc.local
+sudo systemctl restart uwsgi
